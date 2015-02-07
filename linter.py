87c3e080eb367c79212c142d86d9a150bc44853d
Tomas Krehlik <tomas.krehlik@gmail.com>
2015-02-07 22:19:05 +0100

    Improved linter.py, no need for the binaries.

--- linter.py
+++ linter.py
@@ -12,7 +12,7 @@
 
 from SublimeLinter.lint import Linter, util
 from os.path import dirname, realpath
-import sublime
+# import sublime
 
 
 class Julialint(Linter):
@@ -20,15 +20,7 @@ class Julialint(Linter):
     """Provides an interface to julialint."""
 
     syntax = 'julia'
-    plat = sublime.platform()
-    if plat=='osx':
-        cmd = "\"" + dirname(realpath(__file__)) + '/osx.sh\"'
-    elif plat=='linux':
-        cmd = "\"" + dirname(realpath(__file__)) + '/linux.sh\"'
-    else:
-        cmd = "\"" + dirname(realpath(__file__)) + '/windows.bat\"'
-
-    executable = None
+    executable = 'julia'
     regex = r'(?P<file>^.*\.jl):(?P<line>\d{1,4}) \[(?P<func>.*)\] ((?P<error>ERROR)|(?P<warning>WARN)) (?P<message>.*)'
     multiline = False
     line_col_base = (1, 1)
@@ -36,7 +28,17 @@ class Julialint(Linter):
     error_stream = util.STREAM_BOTH
     selectors = {}
     word_re = None
+    comment_re = r'#'
     defaults = {}
     inline_settings = None
     inline_overrides = None
     comment_re = None
+
+    def cmd(self):
+        """Return a list with the command line to execute."""
+
+        command = 'using Lint; lintfile(ARGS[1])'
+        print([self.executable_path, '-e', command])
+        return ['julia',
+                '-e',
+                command]
